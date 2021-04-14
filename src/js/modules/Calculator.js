81a7bfb6b1133eb9c7f3100f6e4743b1c4d83400
Dima <dchaika1990@gmail.com>
2021-04-14 09:53:48 +0300

    fix with dot

--- src/js/modules/Calculator.js
+++ src/js/modules/Calculator.js
@@ -11,8 +11,12 @@ export default class Calculator {
 
 	//Add number to input
 	addNumber(btnValue) {
+		let test = this.input.value;
 		if (+this.input.value === 0) return this.input.value = btnValue;
-		return this.input.value += btnValue;
+		this.input.value += btnValue;
+		if (btnValue === '.' && isNaN(+this.input.value)) {
+			this.input.value = test;
+		}
 	}
 
 	//Calculate percent
